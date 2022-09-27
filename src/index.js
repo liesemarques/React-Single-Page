cabdc14493abb526391f3014c36ee54a3392b53f
liesemarques <liese.m.teixeira@gmail.com>
2022-09-27 09:33:24 -0300

    Sigle page

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import ReactDOM from 'react-dom/client';
+import List from './components/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className='countainer'>
+            <nav className='navbar sticky-top navbar-light bg-dark'>
+                <h1 className='navbar-brand text-light'>Rick and Montry</h1>
+
+            </nav>
+            
+            <List />
+
+          
+            
+        </div>
+        
+        )
+    }
+
+    const container = document.getElementById('app');
+
+    const root = ReactDOM.createRoot(container);
+    
+    root.render(<App />);
+
+
+    //<h1 style={{fontSize: 34}}>Rich and Morty</h1>
